42e588b839916e72d249b003ae8a39f9b41b3fc1
izabella dobielewska <izadobielewska@yahoo.co.uk>
2023-07-10 16:36:08 +0200

    fix template

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function toggleButton() {
   const cards = document.querySelectorAll('[data-js=card]');
   cards.forEach(card => {
     const button = card.querySelector('[data-js="button"]');
-    const link = cards.querySelector('[data-js="link"]');
+    const link = card.querySelector('[data-js="link"]');
     const arrow = card.querySelector('[data-js="arrow"]');
     button.addEventListener('click', () => {
-      link.classList.toggle('hide');
+      link.classList.toggle('hidden');
       arrow.classList.toggle('rotate');
     });
   });
